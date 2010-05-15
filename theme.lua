8725f922464d3bf018849aa91b922b692cdfb9b8
Benoit Pierre <benoit.pierre@gmail.com>
2010-05-16 00:38:21 +0200

    Add theme.

---
+++ theme.lua
@@ -0,0 +1,32 @@
+
+theme = {}
+
+theme.font          = 'Dejavu Sans 9'
+theme.font_mono     = 'Dejavu Sans Mono 9'
+
+theme.bg_normal     = '#111111'
+theme.bg_focus      = '#aacc88'
+theme.bg_urgent     = '#111111'
+
+theme.fg_normal     = '#cccccc'
+theme.fg_focus      = '#000000'
+theme.fg_urgent     = '#cc7766'
+
+theme.border_width  = '1'
+theme.border_normal = '#111111'
+theme.border_focus  = '#333333'
+
+theme.naughty_fg    = '#aacc88'
+theme.naughty_bg    = '#222222'
+theme.naughty_cri   = '#cc7766'
+
+theme.battery_low   = '#ff4444'
+
+theme.text_hilight  = '#aacc88'
+
+theme.wibox_height  = '16'
+
+theme.wallpaper_cmd = { 'xsetroot -solid black' }
+
+return theme
+
