da9072c15c376e5742bc1aa8e9ec42a1abbebd52
Benoit Pierre <benoit.pierre@gmail.com>
2013-09-12 22:27:36 +0200

    Improve Notmuch widget.
    Simplify the code a little: use awful.util.pread.

--- widgets/notmuch.lua
+++ widgets/notmuch.lua
@@ -1,21 +1,16 @@
 -- {{{ Notmuch mail status
 
 local utils = require('widgets.utils')
-
--- Start by checking notmuch is actually present and configured.
-if not os.execute('notmuch config get database.path') then
-  return utils.dummy
-end
+local awutil = require('awful.util')
 
 local timeout = 60
 local notmuch = {}
 
 function notmuch.status()
-  local f = io.popen('notmuch count tag:unread')
-  local s = f:read('*a')
+
+  local s = awutil.pread('notmuch count tag:unread')
   local unread = tonumber(s) or 0
-  local text
-  f:close()
+
   if 0 == unread then
     text = 'no unread mail'
   else
@@ -24,16 +19,27 @@ function notmuch.status()
       text = text .. 's'
     end
   end
+
   return utils.widget_base(text)
+
 end
 
 function notmuch.new()
+
+  -- Start by checking notmuch is actually present and configured.
+  if not os.execute('notmuch config get database.path >/dev/null 2>&1') then
+    return nil
+  end
+
   local w = utils.textbox()
   local t = timer { timeout = timeout }
+
   connect_signal(t, t, 'timeout', function() w:set_markup(notmuch.status()) end)
   t:emit_signal('timeout')
   t:start()
+
   return w
+
 end
 
 return utils.widget_class(notmuch)
