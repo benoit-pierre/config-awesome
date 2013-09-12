55fcbf708b67ee4591ca0703f3423a36a0091eb7
Benoit Pierre <benoit.pierre@gmail.com>
2013-09-12 22:29:29 +0200

    Improve Wibox widget.
    Compact sub-widgets lists: remove nil entries.

--- widgets/wibox.lua
+++ widgets/wibox.lua
@@ -12,16 +12,20 @@ if '3.4' == aw_ver then
     local layout = { layout = awful.widget.layout.horizontal.leftright }
 
     if left_widgets then
-      for i, v in ipairs(left_widgets) do
-        table.insert(layout, v)
+      for k, v in pairs(left_widgets) do
+        if v then
+          table.insert(layout, v)
+        end
       end
     end
 
     if right_widgets then
       local right = {}
       right.layout = awful.widget.layout.horizontal.rightleft
-      for i, v in ipairs(right_widgets) do
-        table.insert(right, v)
+      for k, v in pairs(right_widgets) do
+        if v then
+          table.insert(right, v)
+        end
       end
       table.insert(layout, right)
     end
@@ -39,16 +43,20 @@ if '3.5' == aw_ver then
 
     if left_widgets then
       local left_layout = wibox.layout.fixed.horizontal()
-      for i, v in ipairs(left_widgets) do
-        left_layout:add(v)
+      for k, v in pairs(left_widgets) do
+        if v then
+          left_layout:add(v)
+        end
       end
       layout:set_left(left_layout)
     end
 
     if right_widgets then
       local right_layout = wibox.layout.fixed.horizontal()
-      for i, v in ipairs(right_widgets) do
-        right_layout:add(v)
+      for k, v in pairs(right_widgets) do
+        if v then
+          right_layout:add(v)
+        end
       end
       layout:set_right(right_layout)
     end
