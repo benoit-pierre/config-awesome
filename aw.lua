5e85f660f86eb6426756431c4c99445dc0013680
Benoit Pierre <benoit.pierre@gmail.com>
2014-05-03 17:37:25 +0200

    bindings: add key to toggle 'nofocus' property

--- aw.lua
+++ aw.lua
@@ -682,6 +682,10 @@ awful.key(k_m, 'f', function (c)
     awful.placement.no_offscreen(c)
   end
 end),
+awful.key(k_m, 'x', function (c)
+  local nofocus = not awful.client.property.get(c, 'nofocus')
+  awful.client.property.set(c, 'nofocus', nofocus)
+end),
 awful.key(k_m, 'h', function (c) c.hidden = not c.hidden end),
 awful.key(k_m, 's', function (c) c.sticky = not c.sticky end),
 awful.key(k_m, 't', function (c) c.ontop = not c.ontop end),
